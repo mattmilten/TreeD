8b24eeeace6ad198e76d38962a24abaf0d6c416f
Matthias Miltenberger <matthias.miltenberger@gmail.com>
2021-05-16 16:15:57 +0200

    increase version to 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="treed",
-    version="1.1.0",
+    version="1.2.0",
     author="Matthias Miltenberger",
     author_email="matthias.miltenberger@gmail.com",
     description="3D Visualization of Branch-and-Cut Trees using PySCIPOpt",
